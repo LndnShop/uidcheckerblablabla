d93dfcb3d46b93827d6d336e681924eb3886663c
LndnShop <76145347+LndnShop@users.noreply.github.com>
2025-07-09 13:12:28 +0800

    Update uid.lua

--- uid.lua
+++ uid.lua
@@ -1,6 +1,6 @@
 return {
     ["12345678"] = "Ratih Meisya",
-    ["87654321"] = "Nyoman Kenak",
+    ["958794"] = "DaprD",
     ["765460"] = "DaprD",
     ["22222222"] = "Alsohim",
     ["33333333"] = "Grower",
